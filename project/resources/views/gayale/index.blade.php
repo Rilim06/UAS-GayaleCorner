4d8ee8742f9778d5eea327f4214b11702533dac2
Rilim06 <r.marvinlim@gmail.com>
2023-11-07 20:33:04 +0700

    init

---
+++ project/resources/views/gayale/index.blade.php
@@ -0,0 +1,6 @@
+<head>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">Ini Index</h1>
+</body>
---
+++ project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/views/gayale/index.blade.php',
+    './resources/css/app.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
