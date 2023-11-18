df9b75722abcc139e28e843984b1528e31761918
sshivahh <russelshivah16@gmail.com>
2023-11-18 07:46:20 +0700

    setup database, model, dan controller. Env di example

---
+++ project/resources/views/products/index.blade.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <header>
+            <div class="header-buttons">
+                <a href=""><button>Home</button></a>
+                <a href=""><button>Cart</button></a>
+            </div>
+            <div class="header-search">
+                <input type="text">
+            </div>
+            <div class="header-profile">
+                <p>Hello, {Nama}</p>
+            </div>
+        </header>
+        <footer>
+            <div class="footer-content-left">
+                <h3>Gayale Corner</h3>
+                <p>Bachira is an online store dealing with all product
+                Bachira provide cheap, high quality, products to
+                customers</p>
+            </div>
+            <div class="footer-content-right">
+                <h3>Contact us</h3>
+                <div class="contacts">
+                    <ul>
+                        <li>support@gayalecorner.com</li>
+                        <li>021 - 124 - 456</li>
+                        <li>Summarecon, South Tangerang</li>
+                        <li>@gayalecorner.com</li>
+                        <li>@gayalecorner.com</li>
+                        <li>@gayalecorner.com</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
