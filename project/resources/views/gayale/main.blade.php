42af098085d3346da71fcf1b97c3b2d051b04730
Achxter <hansphilemon@gmail.com>
2023-12-09 21:13:34 +0700

    dashboard navbar

--- project/resources/views/gayale/main.blade.php
+++ project/resources/views/gayale/main.blade.php
@@ -1,11 +1,97 @@
 <head>
     @vite('resources/css/app.css')
     @vite('resources/css/dashboard.css')
+    <!-- @vite('resources/css/navbar.css') -->
+    @vite('resources/js/index.js')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 
 <body>
+    <header class="header bg-white text-black">
+        <nav class="nav container z-20">
+            <div class="nav__data">
+                <a href="#" class="nav__logo ml-8">
+                    <span class="text-[#282828] font-[800]">Gayale</span><span class="text-[#F98538] font-[800]">Corner</span>
+                </a>
+                <div class="nav__toggle mr-20 w-[40px]" id="nav-toggle">
+                    <i class="ri-menu-line nav__burger"></i>
+                    <i class="ri-close-line nav__close"></i>
+                </div>
+            </div>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list ">
+                    <li class="dropdown__item">
+                        <div class="nav__link">
+                            <a class="hover:text-[#F98538]" href="/history">History</a>
+                        </div>
+                        <!-- <ul class="dropdown__menu">
+                            <li>
+                                <a href="#" class="dropdown__link">
+                                    <i class="ri-pie-chart-line"></i> Overview
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" class="dropdown__link">
+                                    <i class="ri-arrow-up-down-line"></i> Transactions
+                                </a>
+                            </li>
+
+                            <li class="dropdown__subitem">
+                                <div class="dropdown__link">
+                                    <i class="ri-bar-chart-line"></i> Reports <i class="ri-add-line dropdown__add"></i>
+                                </div>
+
+                                <ul class="dropdown__submenu">
+                                    <li>
+                                        <a href="#" class="dropdown__sublink">
+                                            <i class="ri-file-list-line"></i> Documents
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" class="dropdown__sublink">
+                                            <i class="ri-cash-line"></i> Payments
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" class="dropdown__sublink">
+                                            <i class="ri-refund-2-line"></i> Refunds
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul> -->
+                    </li>
+                    <li class="dropdown__item">
+                        <div class="nav__link">
+                            <a class="hover:text-[#F98538]" href="/cart">Cart</a>
+                        </div>
+                    </li>
+                    <!-- <li><a href="#" class="nav__link">Products</a></li> -->
+
+                    <li>
+                        <form method="post" action="{{route('logout')}}" class="nav__link p-6">
+                            @csrf
+                            <button class="hover:text-[#F98538]" type='submit'>Logout</button>
+                        </form>
+                    </li>
+                    <!-- <li><a href='/login'>
+                            <h1 class="nav__link font-[700] text-[#F98538] hover:text-white rounded-md bg-transparent hover:bg-[#F98538]">Login</h1>
+                        </a></li>
+                    <li><a href='/register'>
+                            <h1 class="nav__link font-[700] bg-[#F98538] text-white rounded-md hover:text-[#F98538] hover:bg-white" id="designregister">Register</h1>
+                        </a></li> -->
+                    <!-- <li><a href="#" class="nav__link">Contact</a></li> -->
+                </ul>
+            </div>
+        </nav>
+    </header>
+
     <div class='all' id='blur'>
         <div class="">
             @if(session('error'))
