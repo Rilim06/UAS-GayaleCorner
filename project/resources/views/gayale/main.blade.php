9a5c741149a5553509f3cc49a31197da0fec1b64
Achxter <hansphilemon@gmail.com>
2023-12-09 20:07:07 +0700

    dashboard responsive

--- project/resources/views/gayale/main.blade.php
+++ project/resources/views/gayale/main.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     <div class='all' id='blur'>
-        <div class="bg-gray-200 p-4">
+        <div class="">
             @if(session('error'))
             <div class="alert alert-danger">
                 {{ session('error') }}
@@ -24,53 +24,53 @@
                     <a href="/cart">Cart</a>
                 </div>
             </div>
-            <div class="w-[80%] mx-auto">
-                <div class="swiper topSwiper w-full h-[30rem] mt-8 rounded-md">
+            <div class="w-[90%] lg:w-[75%] mx-auto">
+                <div class="swiper topSwiper w-full aspect-square rounded-xl lg:aspect-auto lg:h-[30rem] lg:mt-8 xl:h-[35rem] 2xl:h-[40rem]">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide"><img src="storage/photos/komarin.jpg" alt=""></div>
-                        <div class="swiper-slide"><img src="storage/photos/komari.jpeg" alt=""></div>
-                        <div class="swiper-slide"><img src="storage/photos/Genshin 4.1.jpeg" alt=""></div>
-                        <div class="swiper-slide"><img src="storage/photos/Baizhu Qiqi Web Event Wallpaper.jpg" alt=""></div>
-                        <div class="swiper-slide"><img src="storage/photos/aliongjpeg.jpeg" alt=""></div>
+                        <div class="swiper-slide"><img class="" src="storage/photos/swiper (1).jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="storage/photos/swiper (2).jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="storage/photos/swiper (3).jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="storage/photos/swiper (4).jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="storage/photos/swiper (5).jpg" alt=""></div>
                     </div>
                     <div class="swiper-button-next drop-shadow-lg"></div>
                     <div class="swiper-button-prev drop-shadow-lg"></div>
                     <div class="swiper-pagination"></div>
                 </div>
-                <h3 class="mt-8 font-bold text-2xl text-center">All Categories</h3>
-                <div class="category-grid w-full grid grid-cols-5 gap-4 mt-4">
+                <h3 class="font-bold mt-36 text-7xl sm:text-5xl sm:mt-12 lg:mt-8 lg:text-2xl xl:text-3xl text-center">All Categories</h3>
+                <div class="category-grid flex flex-wrap mt-16 justify-center gap-4 sm:mt-8 lg:w-full lg:grid lg:grid-cols-5 lg:gap-4 lg:mt-4">
                     <a href="category/clothes">
-                        <div class="category-1 mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
-                            <p class="text-white text-lg font-bold align-middle">Clothes</p>
+                        <div class="category-1 w-96 lg:w-full lg:mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
+                            <p class="text-white text-5xl lg:text-lg xl:text-xl font-bold align-middle">Clothes</p>
                         </div>
                     </a>
                     <a href="category/foods">
-                        <div class="category-2 mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
-                            <p class="text-white text-lg font-bold align-middle">Foods</p>
+                        <div class="category-2 w-96 lg:w-full lg:mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
+                            <p class="text-white text-5xl lg:text-lg xl:text-xl font-bold align-middle">Foods</p>
                         </div>
                     </a>
                     <a href="category/beverages">
-                        <div class="category-3 mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
-                            <p class="text-white text-lg font-bold align-middle">Beverages</p>
+                        <div class="category-3 w-96 lg:w-full lg:mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
+                            <p class="text-white text-5xl lg:text-lg xl:text-xl font-bold align-middle">Beverages</p>
                         </div>
                     </a>
                     <a href="category/accessories">
-                        <div class="category-4 mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
-                            <p class="text-white text-lg font-bold align-middle">Accessories</p>
+                        <div class="category-4 w-96 lg:w-full lg:mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
+                            <p class="text-white text-5xl lg:text-lg xl:text-xl font-bold align-middle">Accessories</p>
                         </div>
                     </a>
                     <a href="category/others">
-                        <div class="category-5 mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
-                            <p class="text-white text-lg font-bold align-middle">Others</p>
+                        <div class="category-5 w-96 lg:w-full lg:mt-4 aspect-square flex justify-center items-center rounded-md hover:scale-105 ease-in duration-300">
+                            <p class="text-white text-5xl lg:text-lg xl:text-xl font-bold align-middle">Others</p>
                         </div>
                     </a>
                 </div>
             </div>
-            <h3 class="mt-8 font-bold text-2xl text-center">All Products</h3>
-            <div class="swiper btmSwiper w-full h-min mt-8">
+            <h3 class="font-bold mt-36 text-7xl text-center sm:mt-12 sm:text-5xl lg:mt-8 lg:text-2xl xl:text-3xl">All Products</h3>
+            <div class="swiper btmSwiper w-[90%] h-min mt-16 sm:mt-8 lg:mt-8">
                 <div class="swiper-wrapper">
                     @foreach ($products as $product)
-                    <div class="swiper-slide aspect-square w-36 rounded-lg hover:scale-110 ease-in-out duration-200" onclick='openPopup(
+                    <div class="swiper-slide aspect-square lg:w-36 rounded-xl hover:scale-110 ease-in-out duration-200" onclick='openPopup(
                                         "{{$product->id}}",
                                         "{{$product->name}}",
                                         "{{$product->category}}",
@@ -78,7 +78,7 @@
                                         "Rp.{{$product->price}},-",
                                         "{{asset("storage/" . $product->photo)}}"
                                     )'>
-                        <img class="photo" src="{{asset('storage/' . $product->photo)}}" />
+                        <img class="photo w-full rounded-xl" src="{{asset('storage/' . $product->photo)}}" />
                         <!-- <br /> -->
                         <!-- {{$product->price}}
                             {{$product->category}}
@@ -108,39 +108,44 @@
                     </div>
                 </div>
             </div> -->
+            <div class="footer bg-red-500 mt-36 sm:mt-12">INI FOOTER</div>
         </div>
-        <div class="footer bg-red-500">INI FOOTER</div>
     </div>
     <!-- popup -->
-    <div class="py-8 ps-8 pe-2" id='popup'>
-        <div class="grid grid-cols-2 gap-8">
-            <img class="photo aspect-square" src="" alt="Product Image" />
+    <div class="w-[90%] pt-20 pb-8 px-8 sm:w-[75%] sm:pt-16 sm:px-8 lg:py-8 lg:ps-8 lg:pe-0 rounded-3xl" id='popup'>
+        <div class=" lg:grid lg:grid-cols-2 lg:gap-8">
+            <img class="photo w-full sm:w-3/4 lg:w-full sm:mx-auto aspect-square rounded-xl" src="" alt="Product Image" />
             <div class="flex flex-col justify-between">
-                <div class="">
-                    <h2 id="productName" class="xl:text-3xl font-bold"></h2>
-                    <p id="productCategory" class="xl:text-xl text-slate-500"></p>
-                    <p id="productPrice" class="xl:text-xl font-bold"></p>
+                <div>
+                    <div class="flex flex-col gap-3 mt-8 sm:gap-1 sm:px-20 lg:mt-0 lg:px-0">
+                        <h2 id="productName" class="font-bold text-7xl sm:text-5xl lg:text-3xl xl:text-4xl"></h2>
+                        <p id="productCategory" class="text-slate-500 text-5xl sm:text-4xl lg:text-2xl xl:text-3xl"></p>
+                        <p id="productPrice" class="font-bold text-5xl sm:text-4xl lg:text-2xl xl:text-3xl"></p>
+                    </div>
+                    <p id="productDescription" class="text-slate-500 text-5xl sm:text-3xl sm:px-20 mt-8 lg:px-0 lg:text-[1.3rem] lg:mt-2 xl:mt-4 xl:text-md"></p>
                 </div>
-                <p id="productDescription" class="text-md text-slate-500 mt-4"></p>
                 <form action="/cart" method="post">
                     @csrf
                     <div class="quantity mt-4 flex justify-center items-center gap-4">
-                        <label for="productQuantity">Quantity:</label>
-                        <div class="quantity-controls flex gap-2">
-                            <button type="button" class="quantity-decrease">-</button>
-                            <input class="p-1 w-10 rounded-lg" type="number" id="productQuantity" name="quantity" value="1" min="1">
-                            <button type="button" class="quantity-increase">+</button>
+                        <label class="text-5xl sm:text-4xl lg:text-xl xl:text-2xl" for="productQuantity">Quantity:</label>
+                        <div class="quantity-controls flex gap-4 lg:gap-2">
+                            <button type="button" class="quantity-decrease text-5xl sm:text-4xl lg:text-xl xl:text-2xl">-</button>
+                            <input class="p-1 w-16 rounded-lg text-center text-5xl sm:text-4xl lg:w-10 lg:text-xl xl:text-2xl" type="number" id="productQuantity" name="quantity" value="1" min="1">
+                            <button type="button" class="quantity-increase text-5xl sm:text-4xl lg:text-xl xl:text-2xl">+</button>
                         </div>
                     </div>
                     <input type="hidden" id="productId" name="productId" value="">
                     <input type="hidden" id="productPriceHidden" name="productPrice" value="">
-                    <button type="submit" class="bg-[#F98538] hover:bg-[#ff9854] focus:bg-[#d16f2e] text-white rounded-[32px] flex items-center px-4 py-2 gap-2 mt-8 mx-auto"><img src="storage/photos/Add.svg" alt="">Cart</button>
+                    <button type="submit" class="bg-[#F98538] hover:bg-[#ff9854] focus:bg-[#d16f2e] text-white text-5xl mt-8 mx-auto rounded-full flex items-center gap-4 px-12 py-6 sm:text-4xl lg:text-xl lg:px-4 lg:py-2 lg:gap-2 xl:text-2xl">
+                        <img class="w-12 sm:w-8 lg:w-6 xl:w-5" src="storage/photos/Add.svg" alt="">
+                        <p>Cart</p>
+                    </button>
                 </form>
             </div>
         </div>
         <div class="closing">
             <a class="closing" onclick='openPopup()'>
-                <button class="closeBtn">
+                <button class="closeBtn mt-2">
                     <span class="X"></span>
                     <span class="Y"></span>
                 </button>
@@ -229,21 +234,25 @@
         },
         grabCursor: true,
         loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
         breakpoints: {
             640: {
-                slidesPerView: 2,
+                slidesPerView: 1,
                 spaceBetween: 20,
             },
             768: {
-                slidesPerView: 4,
-                spaceBetween: 40,
+                slidesPerView: 3,
+                spaceBetween: 20,
             },
             1024: {
                 slidesPerView: 5,
+                spaceBetween: 10,
+            },
+            1280: {
+                slidesPerView: 6,
                 spaceBetween: 10,
             },
         },
