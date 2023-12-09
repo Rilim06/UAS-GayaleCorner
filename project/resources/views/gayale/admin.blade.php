74f75d7d8fb3dfc501d43edbcffc283152072735
Achxter <hansphilemon@gmail.com>
2023-12-09 23:11:14 +0700

    admin home all product

--- project/resources/views/gayale/admin.blade.php
+++ project/resources/views/gayale/admin.blade.php
@@ -17,18 +17,18 @@
             <a href="category/accessories">Accessories</a>
             <a href="category/others">Others</a>
             <div class="w-[90%] mx-auto">
-                <a href="gayale/create" class=""><button class="bg-[#08243A] hover:bg-[#08243A] text-white text-center text-3xl py-4 px-8 mt-10 rounded-full lg:text-sm lg:px-5 lg:py-3">
+                <a href="gayale/create" class=""><button class="bg-[#08243A] hover:bg-[#08243A] text-white text-center text-3xl py-4 px-8 mt-10 rounded-full lg:text-sm lg:px-5 lg:py-3 xl:text-md 2xl:text-lg">
                         Tambah Item
                     </button></a>
                 <div class="menu-container mt-8 lg:mt-4">
-                    <div class="w-full grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-4">
+                    <div class="w-full grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-4 2xl:grid-cols-3">
                         @foreach ($products as $product)
-                        <div class="shadow-[0px_3px_10px_0px_rgba(0,0,0,.25)] rounded-2xl px-12 py-8 food-card grid grid-cols-3 gap-12 lg:px-4 lg:py-4">
+                        <div class="shadow-[0px_3px_10px_0px_rgba(0,0,0,.25)] rounded-2xl px-12 py-8 food-card grid grid-cols-3 gap-12 lg:p-4 2xl:p-6">
                             <img class="photo w-full aspect-square rounded-xl" src="{{asset('storage/' . $product->photo)}}" />
                             <div class="flex flex-col my-auto">
-                                <h1 class="text-5xl lg:text-xl">{{$product->name}}</h1>
-                                <h2 class="text-slate-500 text-sm mt-2 lg:mt-0">Category: {{$product->category}}</h2>
-                                <h2 class="font-bold text-lg mt-6 lg:mt-1">Rp. {{$product->price}}</h2>
+                                <h1 class="text-5xl lg:text-xl xl:text-2xl">{{$product->name}}</h1>
+                                <h2 class="text-slate-500 text-sm mt-2 lg:mt-0 xl:text-lg">Category: {{$product->category}}</h2>
+                                <h2 class="font-bold text-lg mt-6 lg:mt-1 xl:text-xl">Rp. {{$product->price}}</h2>
                             </div>
                             <div class="flex flex-col gap-4 justify-center text-center">
                                 <a class="bg-[#F98538] hover:bg-[#ff9854] focus:bg-[#d16f2e] text-white text-2xl p-2 rounded-lg shadow-[0px_2px_8px_0px_rgba(0,0,0,.25)] flex justify-center mx-auto w-1/2 lg:w-full lg:text-lg lg:p-1" href="/gayale/{{$product->id}}/edit">Edit</a>
