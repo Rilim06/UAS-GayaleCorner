d4adc2f96ead318150cac1dc2ad38832bbd4b348
Achxter <hansphilemon@gmail.com>
2023-12-11 02:12:24 +0700

    navbar footer dashboard category

--- project/resources/views/gayale/category.blade.php
+++ project/resources/views/gayale/category.blade.php
@@ -2,112 +2,108 @@
     @vite('resources/css/app.css')
     @vite('resources/css/dashboard.css')
     @vite('resources/js/index.js')
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.7.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 
-<body>
-    <header class="header bg-white text-black">
-        <nav class="nav container z-20">
-            <div class="nav__data">
-                <a href="/gayale" class="nav__logo ml-8">
-                    <span class="text-[#282828] font-[800]">Gayale</span><span class="text-[#F98538] font-[800]">Corner</span>
-                </a>
-                <div class="nav__toggle mr-20 w-[40px]" id="nav-toggle">
-                    <i class="ri-menu-line nav__burger"></i>
-                    <i class="ri-close-line nav__close"></i>
+<body class="relative">
+    <div class='all h-full' id='blur'>
+        <header class="header bg-white text-black">
+            <nav class="nav container z-20">
+                <div class="nav__data">
+                    <a href="/gayale" class="nav__logo ml-8">
+                        <span class="text-[#282828] font-[800]">Gayale</span><span class="text-[#F98538] font-[800]">Corner</span>
+                    </a>
+                    <div class="nav__toggle mr-20 w-[40px]" id="nav-toggle">
+                        <i class="ri-menu-line nav__burger"></i>
+                        <i class="ri-close-line nav__close"></i>
+                    </div>
                 </div>
-            </div>
-
-            <div class="nav__menu" id="nav-menu">
-                <ul class="nav__list ">
-                    <li class="dropdown__item">
-                        <div class="nav__link">
-                            <div class="hover:text-[#F98538]">Categories</div>
-                        </div>
-                        <ul class="dropdown__menu">
-                            <li>
-                                <a href="clothes" class="dropdown__link hover:text-[#F98538]">
-                                    <i class="ri-pie-chart-line"></i> Clothes
-                                </a>
-                            </li>
-                            <li>
-                                <a href="foods" class="dropdown__link hover:text-[#F98538]">
-                                    <i class="ri-arrow-up-down-line"></i> Foods
-                                </a>
-                            </li>
-                            <li>
-                                <a href="beverages" class="dropdown__link hover:text-[#F98538]">
-                                    <i class="ri-arrow-up-down-line"></i> Beverages
-                                </a>
-                            </li>
-                            <li>
-                                <a href="accessories" class="dropdown__link hover:text-[#F98538]">
-                                    <i class="ri-arrow-up-down-line"></i> Accessories
-                                </a>
-                            </li>
-                            <li>
-                                <a href="others" class="dropdown__link hover:text-[#F98538]">
-                                    <i class="ri-arrow-up-down-line"></i> Others
-                                </a>
-                            </li>
-
-                            <!-- <li class="dropdown__subitem">
-                                <div class="dropdown__link">
-                                    <i class="ri-bar-chart-line"></i> Reports <i class="ri-add-line dropdown__add"></i>
-                                </div>
-
-                                <ul class="dropdown__submenu">
-                                    <li>
-                                        <a href="#" class="dropdown__sublink">
-                                            <i class="ri-file-list-line"></i> Documents
-                                        </a>
-                                    </li>
-
-                                    <li>
-                                        <a href="#" class="dropdown__sublink">
-                                            <i class="ri-cash-line"></i> Payments
-                                        </a>
-                                    </li>
-
-                                    <li>
-                                        <a href="#" class="dropdown__sublink">
-                                            <i class="ri-refund-2-line"></i> Refunds
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li> -->
-                        </ul>
-                    </li>
-                    <li class="dropdown__item">
-                        <div class="nav__link">
-                            <a class="hover:text-[#F98538]" href="/history">History</a>
-                        </div>
-                    </li>
-                    <li class="dropdown__item">
-                        <div class="nav__link">
-                            <a class="hover:text-[#F98538]" href="/cart">Cart</a>
-                        </div>
-                    </li>
-                    <!-- <li><a href="#" class="nav__link">Products</a></li> -->
-
-                    <li>
-                        <form method="post" action="{{route('logout')}}" class="nav__link p-6">
-                            @csrf
-                            <button class="hover:text-[#F98538]" type='submit'>Logout</button>
-                        </form>
-                    </li>
-                    <!-- <li><a href='/login'>
-                            <h1 class="nav__link font-[700] text-[#F98538] hover:text-white rounded-md bg-transparent hover:bg-[#F98538]">Login</h1>
-                        </a></li>
-                    <li><a href='/register'>
-                            <h1 class="nav__link font-[700] bg-[#F98538] text-white rounded-md hover:text-[#F98538] hover:bg-white" id="designregister">Register</h1>
-                        </a></li> -->
-                    <!-- <li><a href="#" class="nav__link">Contact</a></li> -->
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <div class='all' id='blur'>
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list ">
+                        <li class="dropdown__item">
+                            <!-- <div class="nav__link"> -->
+                                <div class="text-2xl flex items-center lg:pt-6 lg:text-xl py-1 px-5 hover:text-[#F98538]">Categories</div>
+                            <!-- </div> -->
+                            <ul class="dropdown__menu">
+                                <li>
+                                    <a href="clothes" class="dropdown__link hover:text-[#F98538]">
+                                        <i class="ri-t-shirt-2-fill"></i> Clothes
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="foods" class="dropdown__link hover:text-[#F98538]">
+                                        <i class="ri-restaurant-2-fill"></i> Foods
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="beverages" class="dropdown__link hover:text-[#F98538]">
+                                        <i class="ri-drinks-2-fill"></i> Beverages
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="accessories" class="dropdown__link hover:text-[#F98538]">
+                                        <i class="ri-circle-fill"></i> Accessories
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="others" class="dropdown__link hover:text-[#F98538]">
+                                        <i class="ri-archive-stack-fill"></i> Others
+                                    </a>
+                                </li>
+                                <!-- <li class="dropdown__subitem">
+                                    <div class="dropdown__link">
+                                        <i class="ri-bar-chart-line"></i> Reports <i class="ri-add-line dropdown__add"></i>
+                                    </div>
+    
+                                    <ul class="dropdown__submenu">
+                                        <li>
+                                            <a href="#" class="dropdown__sublink">
+                                                <i class="ri-file-list-line"></i> Documents
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="dropdown__sublink">
+                                                <i class="ri-cash-line"></i> Payments
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="dropdown__sublink">
+                                                <i class="ri-refund-2-line"></i> Refunds
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li> -->
+                            </ul>
+                        </li>
+                        <li class="dropdown__item">
+                            <div class="nav__link">
+                                <a class="hover:text-[#F98538]" href="/history">History</a>
+                            </div>
+                        </li>
+                        <li class="dropdown__item">
+                            <div class="nav__link">
+                                <a class="hover:text-[#F98538]" href="/cart">Cart</a>
+                            </div>
+                        </li>
+                        <!-- <li><a href="#" class="nav__link">Products</a></li> -->
+                        <li>
+                            <form method="post" action="{{route('logout')}}" class="nav__link p-6">
+                                @csrf
+                                <button class="hover:text-[#F98538]" type='submit'>Logout</button>
+                            </form>
+                        </li>
+                        <!-- <li><a href='/login'>
+                                <h1 class="nav__link font-[700] text-[#F98538] hover:text-white rounded-md bg-transparent hover:bg-[#F98538]">Login</h1>
+                            </a></li>
+                        <li><a href='/register'>
+                                <h1 class="nav__link font-[700] bg-[#F98538] text-white rounded-md hover:text-[#F98538] hover:bg-white" id="designregister">Register</h1>
+                            </a></li> -->
+                        <!-- <li><a href="#" class="nav__link">Contact</a></li> -->
+                    </ul>
+                </div>
+            </nav>
+        </header>
         <div class="p-4">
             <!-- <form method="post" action="{{route('logout')}}" class="p-6">
                 @csrf
@@ -143,7 +139,7 @@
                 </div>
             </div>
         </div>
-        <footer class="footer absolute bottom-0 w-full mt-8">
+        <footer class="footer absolute bottom-0 lg:static w-full mt-8 z-0">
             <div class="grid grid-cols-1 lg:grid-cols-3 pl-0 md:pl-8 items-center">
                 <div class="footer-col px-10">
                     <h1 class="text-6xl lg:text-4xl xl:text-3xl"><span class="text-white font-[800]">Gayale</span><span class="text-[#F98538] font-[800]">Corner</span></h1>
@@ -168,7 +164,6 @@
                 </div>
             </div>
         </footer>
-    </div>
     </div>
     <div class="w-[90%] pt-20 pb-8 px-8 sm:w-[75%] sm:pt-16 sm:px-8 lg:py-8 lg:ps-8 lg:pe-0 rounded-3xl" id='popup'>
         <div class=" lg:grid lg:grid-cols-2 lg:gap-8">
